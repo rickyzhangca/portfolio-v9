c989bdeebc2874972d1b8d2f9b707e0980ca58e7
Ricky Zhang <16908811+rickyzhangca@users.noreply.github.com>
2026-01-20 07:52:42 -0500

    ui

--- src/cards/about/about-data.ts
+++ src/cards/about/about-data.ts
@@ -1,5 +1,5 @@
 // Card/sheet size constants for about card
-export const ABOUT_CARD_SIZE = { width: 240, height: 420 };
+export const ABOUT_CARD_SIZE = { width: 240, height: 430 };
 export const ABOUT_SHEET_SIZE = {
   width: 840,
 };
